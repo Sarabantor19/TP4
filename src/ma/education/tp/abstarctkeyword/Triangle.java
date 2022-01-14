5a2e5f58e13629239b4b52e9c88443addc7bbf89
sarabantor19 <sara.bantor19@gmail.com>
2022-01-14 23:13:29 +0100

    commit of the 4th tp

---
+++ src/ma/education/tp/abstarctkeyword/Triangle.java
@@ -0,0 +1,8 @@
+package ma.education.tp.abstarctkeyword;
+
+public  class Triangle extends FormeGeoTypeA{
+    @Override
+    public double calculerSurface() {
+        return 0;
+    }
+}
---
+++ src/ma/education/tp/interfacekeyword/Humain.java
@@ -0,0 +1,14 @@
+package ma.education.tp.interfacekeyword;
+
+public class Humain extends Animal implements Herbivore,Carnivore{
+
+    @Override
+    public void manger(Vegetal Vegetal) {
+
+    }
+
+    @Override
+    public void manger(Animal Animal) {
+
+    }
+}
